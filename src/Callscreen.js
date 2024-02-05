a03062e41149ca2576983bd7e7003835bd42ac39
Abhay Singh <1995.abhaysingh@gmail.com>
2024-02-05 14:41:48 +0530

    final changes

--- src/Callscreen.js
+++ src/Callscreen.js
@@ -6,7 +6,7 @@ import { env } from "./enj.js";
 function Callscreen() {
   const [stream, setStream] = useState();
   const [callerSignal, setCallerSignal] = useState();
-  const [peerConnection, setpeerConnection] = useState()
+  const [peerConnection, setpeerConnection] = useState();
   const remoteVideoRef = useRef();
   const localVideoRef = useRef();
   const socket = useRef();
@@ -14,72 +14,73 @@ function Callscreen() {
 
   socket.current = io.connect(env.REACT_APP_API_KEY);
 
-  useEffect(() => {
+
+  useEffect(() => {    // in useEffect we are getting the media stream of local video feed
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: true })
       .then((stream) => {
         setStream(stream);
-        console.log("local stream", stream.getTracks());
-
-        if (localVideoRef.current) {
-          localVideoRef.current.srcObject = stream;
+        if (localVideoRef.current) { // checking if localVideRef component is mounted  
+          localVideoRef.current.srcObject = stream; // assigning the stream to localVideoRef
         }
-      }).catch(error =>{
-        console.log("catch error" , error)
+      })
+      .catch((error) => {
+        console.log("catch error", error);
       });
 
     return () => {};
   }, []);
 
-  socket.current.on("hey", (data) => {
+  // notifies the user if they are getting the call
+  socket.current.on("incomingCall", (data) => {
     setCallerSignal(data?.signal);
     acceptCall(data?.signal);
   });
 
+  //  this function creates the localDescription and sets to peerConnection and signla the
   const startVideoFeed = () => {
     const peer = new SimplePeer({
-      initiator: true,
-      trickle: false,
-      stream: stream,
+      initiator: true, // identifies that this user(local) is calling
+      trickle: false, // not creating multipl signals
+      stream: stream, // adding stream with session description protocol(sdp)
     });
 
     peer.on("signal", (data) => {
-      socket.current.emit("callUser", { signal: data });
+      // successfully signals the event with ICE candidates
+      socket.current.emit("callUser", { signal: data }); // websockets to call the remote user
     });
 
-   
     socket.current.on("callAccepted", (signal) => {
       peer.signal(signal?.signal);
     });
   };
-  
+
+  // In recevier side, the create the peer after getting the call request
   const acceptCall = (signal) => {
     const peer = new SimplePeer({
-      initiator: false,
-      trickle: false,
-      stream: stream,
+      initiator: false, // identifies that this user(remote) is not calling instead receving
+      trickle: false, //  not emitting multiple singal
+      stream: stream, // adding the local stream(remote) to the peer connection
     });
     peer.on("signal", (data) => {
+      // peer connection establiehes the connection
       socket.current.emit("acceptCall", { signal: data });
     });
-    
-    peer.on("stream", async(stream) => {
-      remoteVideoRef.current.srcObject = stream;
-      const base64Data = await (stream);
-     socket.current.emit('callStarted', stream)
+
+    peer.on("stream", async (stream) => {
+      // getting the stream along with peer connection
+      remoteVideoRef.current.srcObject = stream; //  assigning the audio, video stream to the React DOM
+      socket.current.emit("callStarted", stream); // socket triggered that teh call started
     });
     peer.signal(signal);
-    setpeerConnection(peer)
+    setpeerConnection(peer);
   };
 
-
-
-  
-  const endVideo = () => { 
-    
-    peerConnection?.destroy();
-    remoteVideoRef.current.srcObject = null;
-   }
+  // destroying the peer connection
+  const endVideo = () => {
+    peerConnection?.destroy();  // closing the connection istance
+    remoteVideoRef.current.srcObject = null; // disblaing the streams
+  };
 
   return (
     <div>
@@ -155,7 +156,7 @@ function Callscreen() {
         <Button className="mx-3" onClick={startVideoFeed}>
           Start stream
         </Button>
-        <Button onClick={endVideo} > End stream </Button>
+        <Button onClick={endVideo}> End stream </Button>
       </div>
     </div>
   );
